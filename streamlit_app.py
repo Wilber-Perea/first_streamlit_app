798ca28d2c9fcdfcc89756a5e70bd15621d4e83b
Wilber Perea <wilber.perea@ama.ab.ca>
2022-09-19 17:59:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,6 @@ try:
 except URLerror as e:
   streamlit.error()                   
                     
-streamlit.stop()
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
@@ -61,7 +59,15 @@ if streamlit.button('Get fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
+
+def insert_row_snowflake( new_fruit ):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("INSERT INTO pc_rivery_db.public.fruit_load_list VALUES( '" + new_fruit + "' )")
+    streamlit.write('Thanks for adding ', new_fruit)
   
 add_my_fruit = streamlit.text_input("What fruits would you like to add?")
-streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("INSERT INTO pc_rivery_db.public.fruit_load_list VALUES( '" + add_my_fruit + "' )")
+if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake( add_my_fruit )
+  streamlit.write( back_from_function )
+
